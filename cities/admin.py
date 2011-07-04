37070a10294f22a8c48db9e9e8728b74a862dbe9
Shanto <shanto@hotmail.com>
2011-07-04 03:20:52 -0700

    Edited cities/admin.py via GitHub

--- cities/admin.py
+++ cities/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
+class SearchableAdmin(admin.ModelAdmin):
+	search_fields = ['name']
+
 admin.site.register(Country, SearchableAdmin)
 admin.site.register(Region, SearchableAdmin)
 admin.site.register(City, SearchableAdmin)
