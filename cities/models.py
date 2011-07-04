5d89b26705dc8bb4a35eef9c8973db0264edf135
Shanto <shanto@hotmail.com>
2011-07-04 02:17:23 -0700

    Removed admin specific codes from models.

--- cities/models.py
+++ cities/models.py
@@ -80,8 +80,3 @@ class District(models.Model):
 
 class SearchableAdmin(admin.ModelAdmin):
 	search_fields = ['name']
-
-admin.site.register(Country, SearchableAdmin)
-admin.site.register(Region, SearchableAdmin)
-admin.site.register(City, SearchableAdmin)
-admin.site.register(District, SearchableAdmin)
