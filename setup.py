18f9b38d695b2c32e6827edce51cd15f67f0318a
cash <cash@ragnarok.(none)>
2011-09-21 22:20:43 +0100

    Added classifiers, licence and keywords to setup.py. Fixed typo in MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,10 +1,36 @@
 from setuptools import setup, find_packages
+import os
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(name='cities',
       version='0.1',
       description='Django Cities',
       author='Ben Dowling',
+      author_email='ben.m.dowling@gmail.com',
+      url='https://github.com/coderholic/django-cities',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
+      long_description=read('README'),
+      license = "MIT",
+      keywords = "django cities geonames",
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+
       )
