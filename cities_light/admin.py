c733c796b178d3d571b0fbec74bdf4baffe44eb0
jpic <jamespic@gmail.com>
2012-03-02 18:45:57 +0100

    Rewrite admin.py

--- cities_light/admin.py
+++ cities_light/admin.py
@@ -1,16 +1,62 @@
 from django.contrib import admin
-from models import *
+
+from .models import *
+from .settings import *
 
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'code', 'tld', 'population']
-    search_fields = ['name', 'code', 'tld']
-    
+    list_display = (
+        'name',
+        'code2',
+        'code3',
+        'continent',
+        'tld',
+    )
+    search_fields = (
+        'name',
+        'name_ascii',
+        'code2',
+        'code3',
+        'tld'
+    )
+    list_filter = (
+        'continent',
+    )
 admin.site.register(Country, CountryAdmin)
 
 class CityAdmin(admin.ModelAdmin):
-    ordering = ['name_std']
-    list_display = ['name_std', 'parent', 'population']
-    search_fields = ['name', 'name_std']
-    raw_id_fields = ['region']
-    
-admin.site.register(City, CityAdmin)
+    list_display = (
+        'name',
+        'country',
+    )
+    search_fields = (
+        'name',
+        'name_ascii',
+    )
+    list_filter = (
+        'country__continent',
+        'country',
+    )
+if ENABLE_CITY:
+    admin.site.register(City, CityAdmin)
+
+class PostalCodeAdmin(admin.ModelAdmin):
+    list_display = (
+        'code',
+        'name',
+        'city__name',
+        'city__country__name',
+        'city__country__continent',
+    )
+    search_fields = (
+        'name',
+        'name_ascii',
+        'code',
+        'city__name',
+        'city__name_ascii',
+    )
+    list_filter = (
+        'city__country__continent',
+        'city__country',
+    )
+if ENABLE_POSTAL_CODE:
+    admin.site.register(PostalCode, PostalCodeAdmin)
