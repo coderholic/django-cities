b8b6be8d62d49eece68d59b7bd2a83110ff53482
jpic <jamespic@gmail.com>
2012-03-02 12:11:49 +0100

    Revised the country model, added CONTINENT_CHOICES

--- cities_light/models.py
+++ cities_light/models.py
@@ -5,8 +5,19 @@ import unicodedata
 from django.db.models import signals
 from django.db import models
 from django.template import defaultfilters
+from django.utils.translation import ugettext as _
 
-__all__ = ['Country','City']
+__all__ = ['Country','City', 'CONTINENT_CHOICES']
+
+CONTINENT_CHOICES = (
+    ('OC', _(u'Oceania')),
+    ('EU', _(u'Europe')),
+    ('AF', _(u'Africa')),
+    ('NA', _(u'North America')),
+    ('AN', _(u'Antarctica')),
+    ('SA', _(u'South America')),
+    ('AS', _(u'Asia')),
+)
 
 def ascii_name_and_slug(sender, instance=None, **kwargs):
     if isinstance(instance.name, str):
@@ -23,9 +34,11 @@ class Country(models.Model):
     name_ascii = models.CharField(max_length=200, db_index=True, 
         verbose_name="ascii name", blank=True)
     slug = models.CharField(max_length=200, blank=True)
-    code2 = models.CharField(max_length=2, db_index=True, null=True, blank=True)
-    continent = models.CharField(max_length=2)
-    tld = models.CharField(max_length=5, null=True, blank=True)
+    code2 = models.CharField(max_length=2, blank=True, unique=True)
+    code3 = models.CharField(max_length=3, blank=True, unique=True)
+    continent = models.CharField(max_length=2, db_index=True, choices=CONTINENT_CHOICES)
+    # can't put it unique because of dupe .gp http://en.wikipedia.org/wiki/.bl
+    tld = models.CharField(max_length=5, blank=True, db_index=True)
     
     class Meta:
         ordering = ['name']
