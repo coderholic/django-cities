0a5aeae6e2d875035a5c2098a4b3e63b408d0dee
jpic <jamespic@gmail.com>
2012-03-03 14:14:43 +0100

    Filter out non cities - items without feature PPL
    cities5000.zip and cities1000.zip include cities as well as places. To
    get cities only, a signal reciever was added. The choice of makeing a
    signal reciever rather than to hard code the check is to allow the
    application user to disable that signal and maybe do something else.

--- cities_light/signals.py
+++ cities_light/signals.py
@@ -1,5 +1,12 @@
 import django.dispatch
 
+from exceptions import *
+
 __all__ = ['city_items_pre_import']
 
 city_items_pre_import = django.dispatch.Signal(providing_args=['items'])
+
+def filter_non_cities(sender, items, **kwargs):
+    if 'PPL' not in items[7]:
+        raise InvalidItems()
+city_items_pre_import.connect(filter_non_cities)
