8c08a99c8b0c236ef5ca07ae4e9b89193a718351
jpic <jamespic@gmail.com>
2012-05-09 18:59:25 +0200

    Update to django-autocomplete-light api changes

--- cities_light/static/cities_light/autocomplete_light.js
+++ cities_light/static/cities_light/autocomplete_light.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-    $('.autocompleteselectwidget_light[data-bootstrap=countrycity]').each(function() {
+    $('.autocomplete_light_widget[data-bootstrap=countrycity]').each(function() {
         var country_wrapper = $(this).prev();
         var city_wrapper = $(this);
 
-        country_wrapper.bind('yourlabs_deck.ready', function() {
+        country_wrapper.bind('deckready', function() {
             var city_deck = city_wrapper.yourlabs_deck();
 
-            $(this).yourlabs_deck().options.valueSelect.bind('change', function() {
-                city_deck.options.input.yourlabs_autocomplete().options.data['country__pk'] = $(this).val();
+            $(this).yourlabs_deck().valueSelect.bind('change', function() {
+                city_deck.input.yourlabs_autocomplete().data['country__pk'] = $(this).val();
             });
         });
     });
