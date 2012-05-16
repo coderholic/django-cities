yourlabs/django-cities-light

Import latitude and longitude for Cities
Use a DecimalField so that we do not require geodjango stuff.

cities_light/management/commands/cities_light.py
@@ -167,6 +167,17 @@ It is possible to force the import of files which weren't downloaded using the
             except City.DoesNotExist:
                 city = City(**kwargs)
 
+            save = False
+            if not city.lat:
+                city.lat = items[4]
+                save = True
+            if not city.lng:
+                city.lng = items[5]
+                save = True
+
             if not city.geoname_id: # city may have been added manually
                 city.geoname_id = items[0]
+                save = True
+
+            if save:
                 city.save()

cities_light/models.py
@@ -53,7 +53,12 @@ class City(models.Model):
     name_ascii = models.CharField(max_length=200, db_index=True)
     slug = autoslug.AutoSlugField(populate_from='name_ascii', 
         unique_with=('country__name',))
-    
+
+    lat = models.DecimalField(max_digits=8, decimal_places=5,
+        null=True, blank=True)
+    lng = models.DecimalField(max_digits=8, decimal_places=5,
+        null=True, blank=True)
+
     geoname_id = models.IntegerField(null=True, blank=True)
     country = models.ForeignKey(Country)
 
@@ -64,7 +69,7 @@ class City(models.Model):
 
         if not ENABLE_CITY:
             abstract = True
-        
+
     def __unicode__(self):
         return self.name
 signals.pre_save.connect(set_name_ascii, sender=City)

coderholic/django-cities

cities_light/management/commands/cities_light.py
@@ -167,6 +167,17 @@ It is possible to force the import of files which weren't downloaded using the
             except City.DoesNotExist:
                 city = City(**kwargs)
 
+            save = False
+            if not city.latitude:
+                city.latitude = items[4]
+                save = True
+            if not city.longitude:
+                city.longitude = items[5]
+                save = True
+
             if not city.geoname_id: # city may have been added manually
                 city.geoname_id = items[0]
+                save = True
+
+            if save:
                 city.save()

cities_light/models.py
@@ -53,7 +53,12 @@ class City(models.Model):
     name_ascii = models.CharField(max_length=200, db_index=True)
     slug = autoslug.AutoSlugField(populate_from='name_ascii', 
         unique_with=('country__name',))
-    
+
+    latitude = models.DecimalField(max_digits=8, decimal_places=5,
+        null=True, blank=True)
+    longitude = models.DecimalField(max_digits=8, decimal_places=5,
+        null=True, blank=True)
+
     geoname_id = models.IntegerField(null=True, blank=True)
     country = models.ForeignKey(Country)
 
@@ -64,7 +69,7 @@ class City(models.Model):
 
         if not ENABLE_CITY:
             abstract = True
-        
+
     def __unicode__(self):
         return self.name
 signals.pre_save.connect(set_name_ascii, sender=City)