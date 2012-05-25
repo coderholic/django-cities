e03b40541d8e0843c4585489a2a711ee4183e222
James Pic <jamespic@gmail.com>
2012-05-25 12:12:33 +0200

    Clear the city autocomplete when unselecting the country in countrycity widget

--- cities_light/static/cities_light/autocomplete_light.js
+++ cities_light/static/cities_light/autocomplete_light.js
@@ -19,7 +19,17 @@ $(document).ready(function() {
 
             // set country_pk in city autocomplete data when a country is selected
             country_select.bind('change', function() {
-                city_deck.input.yourlabs_autocomplete().data['country__pk'] = $(this).val();
+                console.log($(this).val());
+                if ($(this).val()) {
+                    city_deck.autocomplete.innerContainer.html('');
+                    city_deck.autocomplete.data['country__pk'] = $(this).val();
+                } else {
+                    city_deck.autocomplete.data['country_pk'] = '';
+                    city_deck.valueSelect.find('option').remove();
+                    city_deck.valueSelect.val('');
+                    city_deck.deck.html('');
+                    city_deck.updateDisplay();
+                }
             });
 
             // set country_pk in city autocomplete data if a country is initially selected
