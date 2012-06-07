66b7ae7f06e1d9a02d9a2dae91815b0648f375b4
James Pic <jamespic@gmail.com>
2012-06-07 17:05:09 +0200

    Fixed restframework contrib

--- cities_light/contrib/restframework.py
+++ cities_light/contrib/restframework.py
@@ -5,6 +5,8 @@ It defines a urlpatterns variables, with the following urls:
 
 - cities_light_api_city_list
 - cities_light_api_city_detail
+- cities_light_api_region_list
+- cities_light_api_region_detail
 - cities_light_api_country_list
 - cities_light_api_country_detail
 
@@ -13,18 +15,17 @@ include::
 
     url(r'^cities_light/api/', include('cities_light.contrib.restframework')),
 
-And there you have a nice API.
+And that's all !
 """
 
 from django.conf.urls.defaults import patterns, url
 from django.core import urlresolvers
-from django.db import models
 
-from djangorestframework.views import *
-from djangorestframework.mixins import *
+from djangorestframework.views import ModelView, ListModelView
+from djangorestframework.mixins import InstanceMixin, ReadModelMixin
 from djangorestframework.resources import ModelResource
 
-from ..models import City, Country
+from ..models import Country, Region, City, to_search
 
 
 class CityResource(ModelResource):
@@ -32,6 +33,27 @@ class CityResource(ModelResource):
     ModelResource for City.
     """
     model = City
+
+    def region(self, instance):
+        """
+        Return the region detail API url.
+        """
+        return urlresolvers.reverse('cities_light_api_region_detail',
+            args=(instance.region.pk,))
+
+    def country(self, instance):
+        """
+        Return the country detail API url.
+        """
+        return urlresolvers.reverse('cities_light_api_country_detail',
+            args=(instance.country.pk,))
+
+
+class RegionResource(ModelResource):
+    """
+    ModelResource for Region.
+    """
+    model = Region
 
     def country(self, instance):
         """
@@ -75,26 +97,6 @@ class LimitListModelView(ListModelView):
             return queryset
 
 
-class CountryListModelView(LimitListModelView):
-    """
-    ModelResource for Country.
-    """
-
-    def get_query_kwargs(self, request, *args, **kwargs):
-        """
-        Converts 'q' to 'name__icontains', allow filter by continent.
-        """
-        kwargs = super(ListModelView, self).get_query_kwargs(request, *args,
-            **kwargs)
-
-        if 'continent' in request.GET.keys():
-            kwargs['continent'] = request.GET['continent']
-        if 'q' in request.GET.keys():
-            kwargs['name__icontains'] = request.GET['q']
-
-        return kwargs
-
-
 class CityListModelView(LimitListModelView):
     """
     ListModelView for City.
@@ -102,24 +104,13 @@ class CityListModelView(LimitListModelView):
 
     def get_query_kwargs(self, request, *args, **kwargs):
         """
-        Allows filtering by GET request arguments:
-
-        - country_name,
-        - country_pk,
-        - continent,
-        - q, for search_names__icontains
+        Allows a GET param, 'q', to be used against search_names.
         """
         kwargs = super(ListModelView, self).get_query_kwargs(request, *args,
             **kwargs)
 
-        if 'country_name' in request.GET.keys():
-            kwargs['country__name'] = request.GET['country_name']
-        if 'country_pk' in request.GET.keys():
-            kwargs['country__pk'] = request.GET['country_pk']
-        if 'continent' in request.GET.keys():
-            kwargs['country__continent'] = request.GET['continent']
         if 'q' in request.GET.keys():
-            kwargs['search_names__icontains'] = request.GET['q']
+            kwargs['search_names__icontains'] = to_search(request.GET['q'])
 
         return kwargs
 
@@ -135,8 +126,18 @@ urlpatterns = patterns('',
         name='cities_light_api_city_detail',
     ),
     url(
+        r'^region/$',
+        ListModelView.as_view(resource=RegionResource),
+        name='cities_light_api_region_list',
+    ),
+    url(
+        r'^region/(?P<pk>[^/]+)/$',
+        DetailView.as_view(resource=RegionResource),
+        name='cities_light_api_region_detail',
+    ),
+    url(
         r'^country/$',
-        CountryListModelView.as_view(resource=CountryResource),
+        ListModelView.as_view(resource=CountryResource),
         name='cities_light_api_country_list',
     ),
     url(
