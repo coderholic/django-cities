15f981bb2a5b27ac4f2741ad1fe37bfcdd21fffd
jpic <jamespic@gmail.com>
2013-12-12 16:33:44 +0100

    Version bump 2.1.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='django-cities-light',
-    version='2.1.6',
+    version='2.1.7',
     description='Simple alternative to django-cities',
     author='James Pic',
     author_email='jamespic@gmail.com',
