8462053abeb1d8d9aba3b4ff772039a75a569890
jpic <jamespic@gmail.com>
2014-04-29 20:26:32 +0200

    Use mysql connector in settings_mysql

--- test_project/test_project/settings_mysql.py
+++ test_project/test_project/settings_mysql.py
@@ -1,17 +1,13 @@
+import django
 from .settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DJANGO_ENGINE', 'django.db.backends.mysql'),
+        'ENGINE': 'mysql.connector.django',
         'NAME': 'cities_light_test',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        'OPTIONS': {
-          'autocommit': True,
-        },
     }
 }
-
-
