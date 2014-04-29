28f776678f4aeb1701b703118e6bbd8a4e434513
jpic <jamespic@gmail.com>
2014-04-29 22:40:42 +0200

    Try using unicode strings everywhere in migrations

--- cities_light/migrations/0002_city.py
+++ cities_light/migrations/0002_city.py
@@ -9,31 +9,31 @@ import cities_light.models
 class Migration(migrations.Migration):
 
     dependencies = [
-        (b'cities_light', b'0001_initial'),
+        ('cities_light', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name=b'City',
+            name='City',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                (b'name_ascii', models.CharField(db_index=True, max_length=200, blank=True)),
-                (b'slug', autoslug.fields.AutoSlugField(editable=False)),
-                (b'geoname_id', models.IntegerField(unique=True, null=True, blank=True)),
-                (b'alternate_names', models.TextField(default=b'', null=True, blank=True)),
-                (b'name', models.CharField(max_length=200, db_index=True)),
-                (b'display_name', models.CharField(max_length=200)),
-                (b'search_names', cities_light.models.ToSearchTextField(default=b'', max_length=4000, db_index=True, blank=True)),
-                (b'latitude', models.DecimalField(null=True, max_digits=8, decimal_places=5, blank=True)),
-                (b'longitude', models.DecimalField(null=True, max_digits=8, decimal_places=5, blank=True)),
-                (b'region', models.ForeignKey(to_field='id', blank=True, to='cities_light.Region', null=True)),
-                (b'country', models.ForeignKey(to='cities_light.Country', to_field='id')),
-                (b'population', models.BigIntegerField(db_index=True, null=True, blank=True)),
-                (b'feature_code', models.CharField(db_index=True, max_length=10, null=True, blank=True)),
+                ('name_ascii', models.CharField(db_index=True, max_length=200, blank=True)),
+                ('slug', autoslug.fields.AutoSlugField(editable=False)),
+                ('geoname_id', models.IntegerField(unique=True, null=True, blank=True)),
+                ('alternate_names', models.TextField(default='', null=True, blank=True)),
+                ('name', models.CharField(max_length=200, db_index=True)),
+                ('display_name', models.CharField(max_length=200)),
+                ('search_names', cities_light.models.ToSearchTextField(default='', max_length=4000, db_index=True, blank=True)),
+                ('latitude', models.DecimalField(null=True, max_digits=8, decimal_places=5, blank=True)),
+                ('longitude', models.DecimalField(null=True, max_digits=8, decimal_places=5, blank=True)),
+                ('region', models.ForeignKey(to_field='id', blank=True, to='cities_light.Region', null=True)),
+                ('country', models.ForeignKey(to='cities_light.Country', to_field='id')),
+                ('population', models.BigIntegerField(db_index=True, null=True, blank=True)),
+                ('feature_code', models.CharField(db_index=True, max_length=10, null=True, blank=True)),
             ],
             options={
-                'ordering': [b'name'],
-                'unique_together': set([(b'region', b'name'), (b'region', b'slug')]),
+                'ordering': ['name'],
+                'unique_together': set([('region', 'name'), ('region', 'slug')]),
                 'abstract': False,
                 'verbose_name_plural': 'cities',
             },
