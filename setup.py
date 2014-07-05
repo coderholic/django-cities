aa9df99a28e28ab10a3444250618b05260f99829
Ben Dowling <ben.m.dowling@gmail.com>
2014-07-05 21:05:03 +0000

    v0.4.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='django-cities',
-    version='0.4',
+    version='0.4.1',
     description='Place models and worldwide place data for Django',
     author='Ben Dowling',
     author_email='ben.m.dowling@gmail.com',
