d3dc4166ef23513916ca5b897f25a4736f613403
jpic <jamespic@gmail.com>
2014-10-31 12:53:09 +0100

    Fix #67: support INDEX_SEARCH_NAMES in django 1.7 migrations

--- cities_light/migrations/0002_city.py
+++ cities_light/migrations/0002_city.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 import autoslug.fields
 import cities_light.models
+from cities_light.settings import INDEX_SEARCH_NAMES
 
 
 class Migration(migrations.Migration):
@@ -23,7 +24,7 @@ class Migration(migrations.Migration):
                 ('alternate_names', models.TextField(default='', null=True, blank=True)),
                 ('name', models.CharField(max_length=200, db_index=True)),
                 ('display_name', models.CharField(max_length=200)),
-                ('search_names', cities_light.models.ToSearchTextField(default='', max_length=4000, db_index=True, blank=True)),
+                ('search_names', models.TextField(default='', max_length=4000, db_index=False, blank=True)),
                 ('latitude', models.DecimalField(null=True, max_digits=8, decimal_places=5, blank=True)),
                 ('longitude', models.DecimalField(null=True, max_digits=8, decimal_places=5, blank=True)),
                 ('region', models.ForeignKey(to_field='id', blank=True, to='cities_light.Region', null=True)),
