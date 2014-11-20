842c92c5048dfaab1670837b314d0ffc47b42c7b
jpic <jamespic@gmail.com>
2014-11-20 10:44:30 +0100

    Make db_index optional

--- cities_light/migrations/0003_auto_20141120_0342.py
+++ cities_light/migrations/0003_auto_20141120_0342.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 import cities_light.models
 
+from cities_light.settings import INDEX_SEARCH_NAMES
+
 
 class Migration(migrations.Migration):
 
@@ -15,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='city',
             name='search_names',
-            field=cities_light.models.ToSearchTextField(default=b'', max_length=4000, db_index=True, blank=True),
+            field=cities_light.models.ToSearchTextField(default=b'', max_length=4000, db_index=INDEX_SEARCH_NAMES, blank=True),
             preserve_default=True,
         ),
     ]
