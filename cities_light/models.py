784a16c2f173d54fe00b7829e439cd985ffbd557
st4lk <myhappydo@gmail.com>
2015-03-30 12:16:15 +0300

    mention in docs usage of named indexes [ci skip]

--- cities_light/models.py
+++ cities_light/models.py
@@ -35,9 +35,10 @@ Steps to customise cities_light models
     .. code:: python
 
         import cities_light
+        from cities_light.settings import ICity
 
         def set_city_fields(sender, instance, items, **kwargs):
-            instance.timezone = items[17]
+            instance.timezone = items[ICity.timezone]
         cities_light.signals.city_items_post_import.connect(set_city_fields)
 
 - Define settings.py:
