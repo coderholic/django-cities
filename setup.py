b4d70fef9d859370fb054b7602dd40a5c0c11274
jpic <jamespic@gmail.com>
2016-01-10 06:58:50 +0100

    Bump 3.2.0 [ci skip]

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name='django-cities-light',
-    version='3.1.2',
+    version='3.2.0',
     description='Simple alternative to django-cities',
     author='James Pic,Dominick Rivard,Alexey Evseev',
     author_email='jamespic@gmail.com, dominick.rivard@gmail.com, myhappydo@gmail.com',
