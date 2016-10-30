b3c8d41daccbde32d50d4d68170b1b0b6665d602
Max Arnold <arnold.maxim@gmail.com>
2016-10-31 00:48:46 +0700

    Move name field to Base model, make not unique

---
+++ cities_light/migrations/0007_make_country_name_not_unique.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cities_light', '0006_compensate_for_0003_bytestring_bug'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='country',
+            name='name',
+            field=models.CharField(max_length=200, db_index=True),
+        ),
+    ]
