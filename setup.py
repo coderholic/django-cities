f853ab1c539fbe185e70c9868f8b63ce49cb067f
Vsevolod Glumov <seva17@gmail.com>
2018-05-27 23:26:17 +0300

    Fix lack of support of Django 2.0 by django_autoslug
    Switched to django-autoslug-iplweb as a more up-to-date alternative to
    django-autoslug.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         'six',
         'pytz',
         'unidecode>=0.04.13',
-        'django_autoslug',
+        'django-autoslug-iplweb==1.9.4.dev0',
         'progressbar2>=3.6.0'
     ],
     classifiers=[
