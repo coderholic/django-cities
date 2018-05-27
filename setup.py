88b897023f5cc7a0d76adaaf7e36343917ea698b
jpic <jamespic@gmail.com>
2018-05-27 22:47:34 +0200

    Release 3.5.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name='django-cities-light',
-    version='3.4.0',
+    version='3.5.0',
     description='Simple alternative to django-cities',
     author='James Pic,Dominick Rivard,Alexey Evseev',
     author_email='jamespic@gmail.com, dominick.rivard@gmail.com, myhappydo@gmail.com',
