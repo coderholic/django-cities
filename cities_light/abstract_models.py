b64491645f8a0cf2eda3415754072f39eed46c07
nguyen <nguyen@codeographer.net>
2020-02-24 14:30:11 +0700

    using six package

--- cities_light/abstract_models.py
+++ cities_light/abstract_models.py
@@ -5,7 +5,7 @@ import re
 import autoslug
 import pytz
 
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 
 from django.db import models
 from django.db.models import lookups
