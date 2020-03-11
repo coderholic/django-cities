a86df7384a8fc130c7019ee86b65dea290926144
Cody Scott <cody.j.b.scott@gmail.com>
2020-03-11 12:09:30 -0400

    Replace ugettext with gettext
    ugettext will be removed in Django 4.0

--- cities/conf.py
+++ cities/conf.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 import django
 from django.conf import settings as django_settings
 from django.core.exceptions import ImproperlyConfigured
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 __all__ = [
     'city_types', 'district_types',
